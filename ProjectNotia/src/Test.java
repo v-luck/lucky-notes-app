96ddcf094546894a4c3a49a129741130cab2f6c0
v-luck <bobbabots@gmail.com>
2020-09-05 22:00:16 -0700

    finished video 4

--- ProjectNotia/src/Test.java
+++ ProjectNotia/src/Test.java
@@ -1,38 +1,45 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class Test extends Application implements EventHandler<ActionEvent> {
-    Button button;
-    Button button2;
+public class Test extends Application {
+
+    Stage window;
+    Scene scene1, scene2;
+
     public static void main(String args[]) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Ai");
-        button = new Button();
-        button.setText("O.O");
-        button.setOnAction(this);
+        window = primaryStage;
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+        Label label1 = new Label("Welcome to the first scene");
+        Button button1 = new Button("Go to scene 2");
+        button1.setOnAction(e -> window.setScene(scene2));
 
-        Scene scene = new Scene(layout, 300, 250);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        // layout 1 - children are laid out in vertical column
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1, button1);
+        scene1 = new Scene(layout1, 200, 200);
+
+        //button 2
+        Button button2 = new Button("This scene sucks, go back to scene 1");
+        button2.setOnAction(e -> window.setScene(scene1));
+
+        //layout 2
+        StackPane layout2 = new StackPane();
+        layout2.getChildren().add(button2);
+        scene2 = new Scene(layout2, 600, 300);
+
+        window.setScene(scene1);
+        window.setTitle("Lit title");
+        window.show();
     }
 
-    @Override
-    public void handle(ActionEvent event) {
-        if(event.getSource() == button) {
-            System.out.println("ouchie");
-            button.setText(">.<");
-        }
-    }
 }
